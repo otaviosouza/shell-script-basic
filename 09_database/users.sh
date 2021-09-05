696736ca1e1e8c4c7b1d65fae33cf397dbfd256f
Souza, Otávio <otavio.souza@toltyosoftware.com.br>
2021-09-05 18:45:47 -0300

    (update): Implemented ListUsers function

--- 09_database/users.sh
+++ 09_database/users.sh
@@ -19,6 +19,7 @@
 # Changelog:
 #   v1.0 05/09/2021, Otávio Souza:
 #       - Error handling
+#       - ListUsers function
 #
 # -----------------------------------------------------------
 # Tested on:
@@ -39,3 +40,25 @@ NC="\033[0m"
 [ ! -e "$DB_FILE" ] && echo -e "${RED}ERROR. The database does not exist.${NC}" && exit 1
 [ ! -r "$DB_FILE" ] && echo -e "${RED}ERROR. Read permission denied.${NC}" && exit 1
 [ ! -w "$DB_FILE" ] && echo -e "${RED}ERROR. Write permission denied.${NC}" && exit 1
+
+ShowUser(){
+    local id
+    local name
+    local email
+    id="$(echo "$1" | cut -d : -f 1)"
+    name="$(echo "$1" | cut -d : -f 2)"
+    email="$(echo "$1" | cut -d : -f 3)"
+
+    echo -e "${GREEN}ID: ${RED}$id${NC}"
+    echo -e "${GREEN}Name: ${RED}$name${NC}"
+    echo -e "${GREEN}E-mail: ${RED}$email${NC}"
+}
+
+ListUsers(){
+    while read -r line
+    do
+        [ "$(echo "$line" | cut -c1)" = "#" ] && continue
+        [ ! "$line" ] && continue
+        ShowUser "$line"
+    done < "$DB_FILE"
+}
