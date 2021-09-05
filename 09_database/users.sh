e0ddb8f16341f65a296948802073cce6722f62bc
Souza, Otávio <otavio.souza@toltyosoftware.com.br>
2021-09-05 18:19:31 -0300

    (update): Error handling

--- 09_database/users.sh
+++ 09_database/users.sh
@@ -29,3 +29,13 @@
 #   your personal time to this course.
 #
 # -----------------------------------------------------------
+
+DB_FILE="users_db.txt"
+
+RED="\033[1;31m"
+GREEN="\033[32m"
+NC="\033[0m"
+
+[ ! -e "$DB_FILE" ] && echo -e "${RED}ERROR. The database does not exist.${NC}" && exit 1
+[ ! -r "$DB_FILE" ] && echo -e "${RED}ERROR. Read permission denied.${NC}" && exit 1
+[ ! -w "$DB_FILE" ] && echo -e "${RED}ERROR. Write permission denied.${NC}" && exit 1
